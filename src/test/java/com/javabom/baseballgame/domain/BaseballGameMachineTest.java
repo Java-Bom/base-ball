4223903e3faae4e3d8a19eca6d280947be677f0b
Malibin <nightmare73@naver.com>
2020-06-16 03:55:11 +0900

    refactor: GameResults 테스트 코드 argument로 테스트 수정

--- src/test/java/com/javabom/baseballgame/domain/BaseballGameMachineTest.java
+++ src/test/java/com/javabom/baseballgame/domain/BaseballGameMachineTest.java
@@ -3,7 +3,6 @@ package com.javabom.baseballgame.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
--- src/test/java/com/javabom/baseballgame/domain/GameResultsTest.java
+++ src/test/java/com/javabom/baseballgame/domain/GameResultsTest.java
@@ -2,25 +2,31 @@ package com.javabom.baseballgame.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class GameResultsTest {
 
-    // EnumList로 strike 3개 들어있을때, 그리고 3개가 아닐때를 실험하고싶은데,
-    // EnumList를 파라미터로 주는 방법은 없나요?
-    @DisplayName("3Strike 이면 야구 게임 성공. true 반환")
-    @Test
-    void isSolved() {
-        // given
-        GameResults gameResults = new GameResults(
-                Arrays.asList(GameResult.STRIKE, GameResult.STRIKE, GameResult.STRIKE)
+    static List<Arguments> gameResults_AndExpectedSolved() {
+        return Arrays.asList(
+                Arguments.of(new GameResults(Arrays.asList(GameResult.STRIKE, GameResult.STRIKE, GameResult.STRIKE)), true),
+                Arguments.of(new GameResults(Arrays.asList(GameResult.STRIKE, GameResult.STRIKE, GameResult.OUT)), false),
+                Arguments.of(new GameResults(Arrays.asList(GameResult.STRIKE, GameResult.BALL, GameResult.OUT)), false)
         );
+    }
 
+    @DisplayName("3Strike 이면 야구 게임 성공, 이외엔 실패.")
+    @ParameterizedTest
+    @MethodSource("gameResults_AndExpectedSolved")
+    void isSolved(GameResults gameResults, boolean expectedResult) {
         // then
-        assertThat(gameResults.isSolved()).isTrue();
+        assertThat(gameResults.isSolved()).isEqualTo(expectedResult);
     }
 
     @DisplayName("게임 결과내에 특정 결과 개수를 제대로 뱉는지 확인")
